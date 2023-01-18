70301ecc7cb770b3aa4dc2ee4eeffb4e290c5ab7
zhangyongfa <1399142039@qq.com>
2023-01-18 10:11:23 +0800

    Update RandomMusic.js

--- RandomMusic.js
+++ RandomMusic.js
@@ -1,12 +1,21 @@
-(function () {
-    var names=['美人画卷(DJ版)','千鸟与目黑 - 叶里','刚烈女子 - 锦零','大风吹','星辰大海','后来'];
 
-    var ids = [1838517951,1339031165,1343283719,1826179038,1811921555,254574];
-    var mid = ids[Math.floor(Math.random() * ids.length)];
+
+(function () {
+
+    var item =
+        [
+            {id: 1842025914, name: '这世界那么多人'}, {id: 1339031165, name: '千鸟与目黑'},
+            {id: 1343283719, name: '刚烈女子'}, {id: 1826179038, name: '大风吹'},
+            {id: 1811921555, name: '星辰大海'}, {id: 1330348068, name: '起风了'},
+            {id: 1479526505, name: '虞兮叹'}, {id: 202369, name: '画'},
+            {id: 29567189, name: '理想'}, {id: 569213220, name: '像我这样的人'}
+        ]
+
+    var mid = item[Math.floor(Math.random() * item.length)].id;
 
     var element = document.getElementById("music");
 
-    element.innerHTML = '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=100% src="//music.163.com/outchain/player?type=2&id='+mid+'&auto=1&height=86"></iframe>';
+    element.innerHTML = '<iframe frameborder="no" border="0" marginwidth="0" marginheight="0" width=100% src="//music.163.com/outchain/player?type=2&id=' + mid + '&auto=1&height=86"></iframe>';
 
 
 })();
